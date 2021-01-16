aec463652e4f543089cba7113602bb0dd0501136
zhang <1770075604@qq.com>
2021-01-16 11:28:53 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -16,7 +16,7 @@ $.ajaxPrefilter(function(options){
 
     // 统一挂载complete回调函数
     options.complete = function(res){
-         if(res.responseJSON.status === 1&& res.responseJSON.message === '身份验证失败'){
+         if(res.responseJSON.status === 1&& res.responseJSON.message === '身份认证失败！'){
                 localStorage.removeItem('token');
                 location.href = '/login.html';
             }
